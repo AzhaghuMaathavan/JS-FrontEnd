9d1083a15c0067a1dc7ab44151905f1aa979edcc
Azmathvn <azhaghupc@gmail.com>
2025-12-19 10:01:25 +0530

    Update FeedbackForm.jsx

--- frontend/src/component/FeedbackForm.jsx
+++ frontend/src/component/FeedbackForm.jsx
@@ -122,3 +122,18 @@ function FeedbackForm() {
 }
 
 export default FeedbackForm;
+
+/*App.js
+import React from "react";
+import FeedbackForm from "./components/FeedbackForm";
+
+function App() {
+  return (
+    <div>
+      <FeedbackForm />
+    </div>
+  );
+}
+
+export default 
+*/
